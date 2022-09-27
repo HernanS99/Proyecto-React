a11d6f8275697edcd0b03bb14c78f482a02637c0
HernanS99 <hernan.silva2299@gmail.com>
2022-09-27 14:39:35 -0300

    fix about us

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -1,10 +1,12 @@
+import Img from '../assets/img/about.jpg'
+
 const Aboutus = () => {
     return (
         <section id="about" class="about">
-            <div class="container">
+            <div class="container mb-5">
                 <div class="row">
                     <div class="col-lg-6">
-                        <img src="../assets/img/abaout.jpg" class="img-fluid" alt=""/>
+                        <img src={Img} class="img-fluid" alt=""/>
                     </div>
                     <div class="col-lg-6 pt-4 pt-lg-0">
                         <h3>About Us</h3>
@@ -12,17 +14,17 @@ const Aboutus = () => {
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         </p>
                         <ul>
-                            <li><i class="bx bx-check-double"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-                            <li><i class="bx bx-check-double"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
+                            <p><i class="fa-solid fa-check-double"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                            <p><i class="fa-solid fa-check-double"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</p>
                         </ul>
                         <div class="row icon-boxes">
                             <div class="col-md-6">
-                                <i class="bx bx-receipt"></i>
+                            <i class="fa-regular fa-cubes"></i>
                                 <h4>Corporis voluptates sit</h4>
                                 <p>Consequuntur sunt aut quasi enim aliquam quae harum pariatur laboris nisi ut aliquip</p>
                             </div>
                             <div class="col-md-6 mt-4 mt-md-0">
-                                <i class="bx bx-cube-alt"></i>
+                            <i class="fa-regular fa-cubes"></i>
                                 <h4>Ullamco laboris nisi</h4>
                                 <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt</p>
                             </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,6 @@
 
 
 function Card(props) {
-    console.log(props)
     return (
             <div class="col-sm-6 col-md-4 col-xl-3">
                 <div class="card-sl mb-5">
@@ -10,7 +9,8 @@ function Card(props) {
                             src="https://images.pexels.com/photos/1149831/pexels-photo-1149831.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt='foto' className="img-fluid"/>
                     </div>
 
-                     <a class="card-action" href="#!"><i class="fa fa-heart"></i></a> 
+                     <a class="card-action" href="#!"><i class="fa fa-heart"></i>
+                     </a> 
                     <div class="card-heading">
                         {props.datos.name}
                     </div>
