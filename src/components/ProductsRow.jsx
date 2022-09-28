4ac7ae4cb5f11641101e313dfb154b45b55d1bec
HernanS99 <hernan.silva2299@gmail.com>
2022-09-28 16:21:32 -0300

    edit 100% :)

--- src/components/ProductsRow.jsx
+++ src/components/ProductsRow.jsx
@@ -1,5 +1,5 @@
 import { db } from '../config/firebase'
-import { doc , deleteDoc} from 'firebase/firestore'
+import { doc , deleteDoc, setDoc} from 'firebase/firestore'
 import { useState } from 'react'
 
 const ProductsRow = (props) => {
@@ -8,7 +8,7 @@ const ProductsRow = (props) => {
     const [edit, setEdit] = useState(false)
     const [epro, setEpro] =  useState({nombre,precio,descripcion})
     const eliminar = async () => {
-        let response = await deleteDoc(doc(db,'products',props.datos.id))
+        await deleteDoc(doc(db,'products',props.datos.id))
     }
     const editt = () => {
         setEdit(true)
@@ -21,17 +21,18 @@ const ProductsRow = (props) => {
         })
     }
 
-    const update = () => {
-        
+    const update = async () => {
+        await setDoc(doc(db,'products',props.datos.id),epro)
+        setEdit(false)
     }
     console.log(epro)
     return (
         <tr>
             <th scope="row">1</th>
-            <td>{edit===false ? nombre :<input id="name" name='name' value={epro.nombre} onChange={(e)=>handleInput(e)}/>}</td>
+            <td>{edit===false ? nombre :<input id="nombre" name='nombre' value={epro.nombre} onChange={(e)=>handleInput(e)}/>}</td>
             <td>{edit===false ? precio :<input id="precio" name='precio' value={epro.precio} onChange={(e)=>handleInput(e)} />} </td>
             <td>{edit===false ? descripcion :<input id="descripcion" name='descripcion' value={epro.descripcion} onChange={(e)=>handleInput(e)}/> }</td>
-            <td><button type="button" className="btn btn-primary" onClick={edit===true ? editt : update}>{edit===false ? 'Editar' : 'Actualizar'}</button></td>
+            <td><button type="button" className="btn btn-primary" onClick={edit===false ? editt : update}>{edit===false ? 'Editar' : 'Actualizar'}</button></td>
             <td>{edit===false ? <button type="button" className="btn btn-primary" onClick={eliminar}>Eliminar</button>: null}</td>
         </tr>
     )
