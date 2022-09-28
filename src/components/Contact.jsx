4b2464f4a166a3b47a4cf9d4d9e7d1df989ac3e2
HernanS99 <hernan.silva2299@gmail.com>
2022-09-28 00:42:02 -0300

    contanct context ready

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,28 +9,28 @@ const Contact = () => {
                                     <legend className="text-center">Contact us</legend>
 
                                     <div className="form-group">
-                                        <label className="control-label" for="name">Name</label>
+                                        <label className="control-label" for="name">Nombre</label>
                                         <div className="">
-                                            <input id="name" name="name" type="text" placeholder="Your name" className="form-control" />
+                                            <input id="name" name="name" type="text" placeholder="Tu nombre" className="form-control" />
                                         </div>
                                     </div>
                                     <div className="form-group">
-                                        <label className="control-label" for="email">Your E-mail</label>
+                                        <label className="control-label" for="email">Tu Correo</label>
                                         <div className="">
-                                            <input id="email" name="email" type="text" placeholder="Your email" className="form-control" />
+                                            <input id="email" name="email" type="text" placeholder="Tu correo" className="form-control" />
                                         </div>
                                     </div>
 
                                     <div className="form-group mb-4">
-                                        <label className="control-label" for="message">Your message</label>
+                                        <label className="control-label" for="message">Tu Mensaje</label>
                                         <div className="">
-                                            <textarea className="form-control" id="message" name="message" placeholder="Please enter your message here..." rows="5"></textarea>
+                                            <textarea className="form-control" id="message" name="message" placeholder="Por favor ingresa tu mensaje aqui..." rows="5"></textarea>
                                         </div>
                                     </div>
 
                                     <div className="form-group">
                                         <div className="col-md-12 text-right">
-                                            <button type="submit" className="btn btn-primary btn-lg">Submit</button>
+                                            <button type="submit" className="btn btn-primary btn-lg">Enviar</button>
                                         </div>
                                     </div>
                                 </fieldset>
