96ec6a9e738aec3a22901bbb4d5001be07e1ed07
HernanS99 <hernan.silva2299@gmail.com>
2022-09-28 15:22:06 -0300

    add delete products

--- src/components/ProductsRow.jsx
+++ src/components/ProductsRow.jsx
@@ -1,11 +1,14 @@
 import { db } from '../config/firebase'
-import { collection , deleteDoc} from 'firebase/firestore'
+import { doc , deleteDoc} from 'firebase/firestore'
 
 const ProductsRow = (props) => {
     let datos = props.datos.data()
     const {nombre, precio, descripcion} = datos
 
-    
+    const eliminar = async () => {
+        let response = await deleteDoc(doc(db,'products',props.datos.id))
+        console.log(response)
+    }
 
     console.log(datos)
     return (
@@ -14,8 +17,8 @@ const ProductsRow = (props) => {
             <td>{nombre}</td>
             <td>{precio}</td>
             <td>{descripcion}</td>
-            <td><button type="button" class="btn btn-primary">Editar</button></td>
-            <td><button type="button" class="btn btn-primary">Eliminar</button></td>
+            <td><button type="button" className="btn btn-primary">Editar</button></td>
+            <td><button type="button" className="btn btn-primary" onClick={eliminar}>Eliminar</button></td>
         </tr>
     )
 }
