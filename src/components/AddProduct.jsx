7c425251ef89d156d43aef3b6e5a4fde47a757ce
HernanS99 <hernan.silva2299@gmail.com>
2022-09-28 12:20:27 -0300

    add insert addproduct

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -1,35 +1,42 @@
 import { useState } from "react"
+import { db } from '../config/firebase'
+import { collection , addDoc} from 'firebase/firestore'
 
 const AddProduct = () => {
 
     const [product, saveProduct] = useState({})
 
     const handleInput = (e) => {
-        const {value , name } = e.target
+        const  { value , name } = e.target
         saveProduct({
             ...product,
-            name: value
+            [name]: value
         }
         )
     }
 
+    const saveProductos = async () => {
+        await addDoc(collection(db,'products'),product)
+    }
+
+    console.log(product)
     return (
         <div className="container">
             <div className="row">
                 <div class="input-group mb-3 mt-5">
                     <p className="me-3">Nombre: </p>
-                    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
+                    <input type="text" class="form-control" name="nombre" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" onChange={(evento) => handleInput(evento)}/>
                 </div>
                 <div class="input-group mb-3">
                     <p className="me-3">Precio: </p>
-                    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
+                    <input type="number" class="form-control" name="precio" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" onChange={(evento) => handleInput(evento)}/>
                 </div>
                 <div class="input-group mb-3">
                     <p className="me-3">Descripcion: </p>
-                    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
+                    <input type="text" class="form-control" name="descripcion" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" onChange={(evento) => handleInput(evento)}/>
                 </div>
                 <div className="col-md-12 text-center mb-3">
-                    <button type="button" className="btn btn-primary btn-lg">Enviar</button>
+                    <button type="button" className="btn btn-primary btn-lg" onClick={saveProductos}>Enviar</button>
                 </div>
             </div>
         </div>
