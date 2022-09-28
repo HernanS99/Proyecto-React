679d7f6849c1030305b427afc86d106694548e5e
HernanS99 <hernan.silva2299@gmail.com>
2022-09-28 12:10:52 -0300

    add button and handleinput addproduct

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -1,4 +1,18 @@
+import { useState } from "react"
+
 const AddProduct = () => {
+
+    const [product, saveProduct] = useState({})
+
+    const handleInput = (e) => {
+        const {value , name } = e.target
+        saveProduct({
+            ...product,
+            name: value
+        }
+        )
+    }
+
     return (
         <div className="container">
             <div className="row">
@@ -10,10 +24,13 @@ const AddProduct = () => {
                     <p className="me-3">Precio: </p>
                     <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
                 </div>
-                    <div class="input-group mb-3">
-                        <p className="me-3">Descripcion: </p>
-                        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
-                    </div>
+                <div class="input-group mb-3">
+                    <p className="me-3">Descripcion: </p>
+                    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
+                </div>
+                <div className="col-md-12 text-center mb-3">
+                    <button type="button" className="btn btn-primary btn-lg">Enviar</button>
+                </div>
             </div>
         </div>
 
