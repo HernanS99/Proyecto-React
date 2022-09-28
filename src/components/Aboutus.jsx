9b51a39528adb8ce2a78214322ae94ecf379561d
HernanS99 <hernan.silva2299@gmail.com>
2022-09-28 00:19:15 -0300

    add all content about us

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -2,31 +2,31 @@ import Img from '../assets/img/team.jpg'
 
 const Aboutus = () => {
     return (
-        <section id="about" class="about">
-            <div class="container mb-5">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <img src={Img} class="img-fluid" alt=""/>
+        <section id="about" className="about">
+            <div className="container mb-5">
+                <div className="row">
+                    <div className="col-lg-6">
+                        <img src={Img} className="img-fluid" alt=""/>
                     </div>
-                    <div class="col-lg-6 pt-4 pt-lg-0">
+                    <div className="col-lg-6 pt-4 pt-lg-0">
                         <h3>Nosotros</h3>
                         <p>
                         Tenemos todo dispuesto para entregar una experiencia única.
                         </p>
                         <ul>
-                            <p><i class="fa-solid fa-check-double"></i> Directamente desde China nuestros chefs te cautivan con gastronomía tradicional de la región, amoldada a los gustos particulares del paladar santiaguino, prevaleciendo el interés principal de satisfacer el gusto, olfato y vista de nuestros clientes.</p>
-                            <p><i class="fa-solid fa-check-double"></i> Contamos con una variada carta de comida china.</p>
+                            <p><i className="fa-solid fa-check-double"></i> Directamente desde China nuestros chefs te cautivan con gastronomía tradicional de la región, amoldada a los gustos particulares del paladar santiaguino, prevaleciendo el interés principal de satisfacer el gusto, olfato y vista de nuestros clientes.</p>
+                            <p><i className="fa-solid fa-check-double"/> Contamos con una variada carta de comida china.</p>
                         </ul>
-                        <div class="row icon-boxes">
-                            <div class="col-md-6">
-                            <i class="fa-regular fa-cubes"></i>
-                                <h4>Corporis voluptates sit</h4>
-                                <p>Consequuntur sunt aut quasi enim aliquam quae harum pariatur laboris nisi ut aliquip</p>
+                        <div className="row icon-boxes">
+                            <div className="col-md-6">
+                            <i className="fa-regular fa-cubes"/>
+                                <h4>Calidad</h4>
+                                <p>Contamos con ingredientes de calidad premium para darle las mejores comidas</p>
                             </div>
-                            <div class="col-md-6 mt-4 mt-md-0">
-                            <i class="fa-regular fa-cubes"></i>
-                                <h4>Ullamco laboris nisi</h4>
-                                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt</p>
+                            <div className="col-md-6 mt-4 mt-md-0">
+                            <i className="fa-regular fa-cubes"/>
+                                <h4>Tiempo de entrega</h4>
+                                <p>Tenemos el promedio de tiempo de entrega más corto de la region</p>
                             </div>
                         </div>
                     </div>
