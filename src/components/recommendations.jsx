6bbdd1be2b5ce8a45f136d33e5e0c78c81533519
HernanS99 <hernan.silva2299@gmail.com>
2022-10-02 16:27:06 -0300

    add recommendations

--- src/components/recommendations.jsx
+++ src/components/recommendations.jsx
@@ -1,11 +1,33 @@
 import { db } from '../config/firebase'
-import { collection , getDocs} from 'firebase/firestore'
+import { collection, getDocs } from 'firebase/firestore'
 import { useEffect, useState } from "react"
 
 const recommendations = () => {
-    return(
-        <div>
-            
+    return (
+        <div class="container">
+            <section class="mx-auto my-5" style="max-width: 23rem;">
+
+                <div class="card">
+                    <div class="card-body">
+                        <blockquote class="blockquote blockquote-custom bg-white px-3 pt-4">
+                            <div class="blockquote-custom-icon bg-info shadow-1-strong">
+                                <i class="fa fa-quote-left text-white"></i>
+                            </div>
+                            <p class="mb-0 mt-2 font-italic">
+                                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                                exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                <a href="#" class="text-info">@consequat</a>."
+                            </p>
+                            <footer class="blockquote-footer pt-4 mt-4 border-top">
+                                Someone famous in
+                                <cite title="Source Title">Source Title</cite>
+                            </footer>
+                        </blockquote>
+                    </div>
+                </div>
+
+            </section>
         </div>
     )
 }
