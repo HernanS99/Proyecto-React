2e02cda670816f003476b32e13dd5c563644b862
HernanS99 <hernan.silva2299@gmail.com>
2022-10-02 16:42:54 -0300

    recommendations and admin view

--- src/components/recommendations.jsx
+++ src/components/recommendations.jsx
@@ -1,11 +1,11 @@
-import { db } from '../config/firebase'
-import { collection, getDocs } from 'firebase/firestore'
-import { useEffect, useState } from "react"
 
-const recommendations = () => {
+
+const Recommendations = (props) => {
+    let datos = props.datos.data()
+    const {email,name,message} = datos
     return (
         <div class="container">
-            <section class="mx-auto my-5" style="max-width: 23rem;">
+            <section class="mx-auto my-5">
 
                 <div class="card">
                     <div class="card-body">
@@ -13,15 +13,14 @@ const recommendations = () => {
                             <div class="blockquote-custom-icon bg-info shadow-1-strong">
                                 <i class="fa fa-quote-left text-white"></i>
                             </div>
+                            <p class="fs-2">
+                                Nombre: {name}
+                            </p>
                             <p class="mb-0 mt-2 font-italic">
-                                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                                exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                <a href="#" class="text-info">@consequat</a>."
+                                {message}
                             </p>
                             <footer class="blockquote-footer pt-4 mt-4 border-top">
-                                Someone famous in
-                                <cite title="Source Title">Source Title</cite>
+                                <cite title="Source Title">{email}</cite>
                             </footer>
                         </blockquote>
                     </div>
@@ -32,4 +31,4 @@ const recommendations = () => {
     )
 }
 
-export default recommendations
+export default Recommendations
--- src/views/AdminPage.jsx
+++ src/views/AdminPage.jsx
@@ -1,9 +1,32 @@
 import ProductsMenu from "../components/ProductsMenu"
+import Recommendations from "../components/Recommendations"
+import { db } from '../config/firebase'
+import { collection, getDocs } from 'firebase/firestore'
+import { useEffect, useState } from "react"
 
 const AdminPage = () => {
+    const [recom, setRecom] = useState([])
+    
+
+
+    const getProducts = async () => {
+        try{
+            let response = await getDocs(collection(db,'recomendaciones'))
+            setRecom(response.docs)
+        }catch(err){
+            
+        }
+    }
+
+    useEffect(()=>{
+        getProducts()
+    },[])
+
     return(
         <div>
+            {recom.map(recomendacion =><Recommendations datos={recomendacion} key={recomendacion.id}/>)}
             <ProductsMenu/>
+            
         </div>
     )
 }
