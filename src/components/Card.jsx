9e9c8c99082e5a7c4c8642f285acdb4752fe6ac1
HernanS99 <hernan.silva2299@gmail.com>
2022-10-02 16:16:47 -0300

    add img to card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,13 +2,13 @@
 
 function Card(props) {
     let datos = props.datos.data()
-    const {nombre, precio, descripcion} = datos
+    const {nombre, precio, descripcion,img} = datos
     return (
             <div className="col-sm-6 col-md-4 col-xl-3">
                 <div className="card-sl mb-5">
                     <div className="card-image">
                         <img
-                            src="https://images.pexels.com/photos/1149831/pexels-photo-1149831.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt='foto' classNameName="img-fluid"/>
+                            src={img} alt='foto' classNameName="img-fluid"/>
                     </div>
                     <div className="card-heading">
                         {nombre}
