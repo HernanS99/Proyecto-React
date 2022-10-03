1e02fff899e5e96365639050dda9f8fdf6c46e23
HernanS99 <hernan.silva2299@gmail.com>
2022-10-03 17:31:59 -0300

    add login logic

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom'
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from "./config/firebase";
 import Home from './views/Home';
 import NotFound from './views/NotFound';
 import AdminPage from './views/AdminPage';
 import Register from './views/Register'
 import Login from './views/Login';
 import Nav from './components/Nav';
+import { useState } from 'react';
+
 
 
 
 function App() {
+
+  const [user, setUser] = useState(null)
+
+
+  onAuthStateChanged(auth, (usuario=>{
+    if(usuario)
+    {
+      setUser(usuario)
+    }else{
+      setUser(null)
+    }
+  }))
+
   return (
     <div>
       <Nav>
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='*' element={<NotFound />} />
+          {user === null ? <Route path='*' element={<NotFound />} />: null}
+          
           <Route path='/AdminPage' element={<AdminPage />} />
           <Route path='/Register' element={<Register />} />
           <Route path='/Login' element={<Login />} />
