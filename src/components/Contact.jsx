ad1f4280cd1d7f08dd8d7f97a38517496adb48c5
HernanS99 <hernan.silva2299@gmail.com>
2022-10-04 16:32:48 -0300

    delete all for

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -47,22 +47,22 @@ const Contact = () => {
                                     <legend className="text-center">Contactanos</legend>
 
                                     <div className="form-group">
-                                        <label className="control-label" for="name">Nombre</label>
+                                        <label className="control-label" >Nombre</label>
                                         <div className="">
                                             <input id="name" name="name" type="text" placeholder="Tu nombre" className="form-control" onChange={(evento) => handleInput(evento)}/>
                                         </div>
                                     </div>
                                     <div className="form-group">
-                                        <label className="control-label" for="email">Tu Correo</label>
+                                        <label className="control-label" >Tu Correo</label>
                                         <div className="">
                                             <input id="email" name="email" type="email" placeholder="Tu correo" className="form-control" onChange={(evento) => handleInput(evento)}/>
                                         </div>
                                     </div>
 
                                     <div className="form-group mb-4">
-                                        <label className="control-label" for="message">Tu Mensaje</label>
+                                        <label className="control-label">Tu Mensaje</label>
                                         <div className="">
-                                            <textarea className="form-control" id="message" name="message" placeholder="Por favor ingresa tu mensaje aqui..." rows="5" onChange={(evento) => handleInput(evento)}></textarea>
+                                            <textarea className="form-control" id="message" name="message" placeholder="Por favor ingresa tu mensaje aqui..." ength="3minl0" rows="5" onChange={(evento) => handleInput(evento)}></textarea>
                                         </div>
                                     </div>
 
