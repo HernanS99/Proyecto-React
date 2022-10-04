2f748c2e81844cd890262aab76fe3ca731372cec
HernanS99 <hernan.silva2299@gmail.com>
2022-10-04 16:17:28 -0300

    50 % navbar fix

--- src/components/NavMax.jsx
+++ src/components/NavMax.jsx
@@ -1,12 +1,28 @@
 import { useNavigate } from 'react-router-dom';
 import { Router, Route} from 'react-router'
 import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from "../config/firebase";
+
 function NavMax() {
 
   const navigate = useNavigate();
   
+  const [user, setUser] = useState(null)
+  onAuthStateChanged(auth, (usuario=>{
+    if(usuario)
+    {
+      setUser(usuario)
+    }else{
+      setUser(null)
+    }
+  }))
+
 
   return (
+
+    
     <nav className="navbar navbar-expand-lg bg-light">
       <div className="container">
         <img className="navbar-brand" src="https://banner2.cleanpng.com/20180419/ptw/kisspng-japanese-cuisine-icon-sushi-computer-icons-sake-a-pair-of-chopsticks-5ad8ae8115d850.3968730015241498890895.jpg" width="80" height="54" alt="" />Restorant Comida China
@@ -18,9 +34,16 @@ function NavMax() {
             <li className="nav-item">
             <a className="nav-link active" aria-current="page"><Link to="/">Inicio</Link></a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page"><Link to="/Login">Login</Link></a>
+            {user === null ?
+              <li className="nav-item">
+              <a className="nav-link active" aria-current="page"><Link to="/Login">Administracion</Link></a>
             </li>
+            :
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page"><Link to="/Login">Administracion</Link></a>
+            </li>
+            }
+            
           </ul>
         </div>
       </div>
